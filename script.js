0b88c13b91c18bdfa6c86b7ba0284b7a10adf958
rihab <rihabsalmi2609@gmail.com>
2024-01-16 12:10:39 +0100

    changed colors

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function rollDice() {
+  let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+  let newSrc1 = "images/dice" + randomNumber1 + ".png";
+  let newSrc2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelector(".img1").setAttribute("src", newSrc1);
+  document.querySelector(".img2").setAttribute("src", newSrc2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "🏆 Player 1 wins";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector(".container h1").innerHTML = "🏆 Player 2 wins";
+  } else {
+    document.querySelector(".container h1").innerHTML = "Draw!";
+  }
+}
